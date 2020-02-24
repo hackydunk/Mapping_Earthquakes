1082aed16d6f657d6552965d8a14616c98ba7cea
unknown <hackbarth.jd@gmail.com>
2020-02-23 21:05:45 -0800

    completing linestrings

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,20 +1,37 @@
 // Add console.log to check to see if our code is working.
 console.log('working');
 
-// Accessing the airport GeoJSON URL
-let airportData =
-  'https://raw.githubusercontent.com/hackydunk/Mapping_Earthquakes/master/majorAirports.json';
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData =
+  'https://raw.githubusercontent.com/hackydunk/Mapping_Earthquakes/master/torontoRoutes.json';
+
+// Create a style for the lines.
+let myStyle = {
+  color: '#ffffa1',
+  weight: 2
+};
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: (feature, layer) => {
+      layer.bindPopup(
+        '<h3>Airline: ' +
+          feature.properties.airline +
+          '</h3> <hr><h3> Destination: ' +
+          feature.properties.dst +
+          '</h3>'
+      );
+    }
+  }).addTo(map);
 });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer(
-  'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
+let light = L.tileLayer(
+  'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',
   {
     attribution:
       'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -36,15 +53,15 @@ let dark = L.tileLayer(
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44.0, -80.0],
   zoom: 2,
-  layers: [streets]
+  layers: [light]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
